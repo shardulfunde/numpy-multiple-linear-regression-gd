f017f4c64ce426704cae536ccf071299fdd777f2
Shardul Funde <fundeshardul@gmail.com>
2026-08-16 16:52:42 +0530

    Step 2: split_train_val_test

--- model.py
+++ model.py
@@ -15,8 +15,22 @@ def shuffle_xy(X, y, seed=42):
     y=y[indices]
     return X,y
 
-# Step 2 - split_train_val_test (not yet solved)
-# TODO: implement
+# Step 2 - split_train_val_test
+def split_train_val_test(X, y, train_frac=0.6, val_frac=0.2):
+    n=X.shape[0]
+    n_train=int(n*train_frac)
+    n_val=int(n*val_frac)
+    n_test=n-n_train-n_val
+
+    X_train=X[:n_train,:]
+    y_train=y[:n_train]
+    
+    X_val=X[n_train:n_train+n_val,:]
+    y_val=y[n_train:n_train+n_val]
+
+    X_test=X[n-n_test:,:]
+    y_test=y[n-n_test:]
+    return X_train, y_train, X_val, y_val, X_test, y_test
 
 # Step 3 - compute_feature_stats (not yet solved)
 # TODO: implement
