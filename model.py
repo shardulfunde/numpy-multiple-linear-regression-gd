b3e57bbe5221491ced71b45ba7b1d4d71f1a3196
Shardul Funde <fundeshardul@gmail.com>
2026-08-16 17:04:12 +0530

    Step 4: standardize_features

--- model.py
+++ model.py
@@ -39,8 +39,10 @@ def compute_feature_stats(X):
     std[std==0]=1
     return mean,std
 
-# Step 4 - standardize_features (not yet solved)
-# TODO: implement
+# Step 4 - standardize_features
+def standardize_features(X, mean, std):
+    X_scaled=(X-mean)/std
+    return X_scaled
 
 # Step 5 - add_bias_column (not yet solved)
 # TODO: implement
