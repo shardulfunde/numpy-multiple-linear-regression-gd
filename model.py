a9cfe6ba9908a804214909758b9aeebbed78fbf3
Shardul Funde <fundeshardul@gmail.com>
2026-08-18 13:11:39 +0530

    Step 21: evaluate_regression

--- model.py
+++ model.py
@@ -159,8 +159,13 @@ def r_squared(y_true, y_pred):
     else:
         return 1-(residual_squared/sum_squared)
 
-# Step 21 - evaluate_regression (not yet solved)
-# TODO: implement
+# Step 21 - evaluate_regression
+def evaluate_regression(y_true, y_pred):
+    return {
+        "mae":mean_absolute_error(y_true,y_pred),
+        "rmse":root_mean_squared_error(y_true,y_pred),
+        "r2":r_squared(y_true,y_pred)
+    }
 
 # Step 22 - learning_curve_data (not yet solved)
 # TODO: implement
