6ef1deb662dc94d05788ae752db6bca6ee3dd2f8
Shardul Funde <fundeshardul@gmail.com>
2026-08-18 14:59:36 +0530

    Step 28: compare_with_normal_equation

--- model.py
+++ model.py
@@ -237,6 +237,9 @@ def score_lr_model(model, X, y):
     y_hat=predict_lr_model(model,X)
     return evaluate_regression(y,y_hat)
 
-# Step 28 - compare_with_normal_equation (not yet solved)
-# TODO: implement
+# Step 28 - compare_with_normal_equation
+def compare_with_normal_equation(model):
+    weights=model["weights"]
+    normal_weights=model["normal_weights"]
+    return weights_l2_distance(weights,normal_weights)
 
